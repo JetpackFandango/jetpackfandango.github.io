06cf76454c97e226f1e6a923c3c5e12bead88440
Matthew Hudgins <matthew@interactivestrategies.com>
2023-01-19 16:03:25 -0500

    hide tile

---
+++ js/hide-sketch.js
@@ -0,0 +1,69 @@
+let nGrid = [];
+let eGrid = [];
+let sGrid = [];
+let wGrid = [];
+let cols = 7;
+let rows = 2;
+let timerValue = 20;
+let num_found = 0;
+let max_blue = 0;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  nGrid = makeGrid();
+  eGrid = makeGrid(150);
+  sGrid = makeGrid(300);
+  wGrid = makeGrid(450);
+  textSize(18);
+  
+}
+function mousePressed(){
+  checkClick(nGrid);
+  checkClick(eGrid);
+  checkClick(sGrid);
+  checkClick(wGrid);
+}
+function checkClick(thisGrid){
+    for(let r = 0; r < rows; r++){
+    for(let c = 0; c < cols; c++){
+      thisGrid[r][c].click();
+    }
+  }
+}
+function makeGrid(yoffset = 0, xoffset = 10){
+  let grid = [];
+  for(let r = 0; r < rows; r++){
+    grid[r] = [];
+    for(let c = 0; c < cols; c++){
+      let x = r * 50; // x position of the square
+      let y = c * 50;
+      x = x + yoffset;
+      y = y + xoffset;
+      grid[r][c] = new Tile(x,y,c,r);
+    }
+  }
+  return grid;
+}
+function draw() {
+  background(255);
+  drawGrid(nGrid);
+  text("N",352,50);
+  drawGrid(eGrid);
+  text("E",352,200);
+  drawGrid(sGrid);
+  text("S",352,350);
+  
+  drawGrid(wGrid);
+  text("W",352,500);
+  
+  
+}
+
+
+function drawGrid(thisGrid) {
+  for(let r = 0; r < rows; r++){
+    for(let c = 0; c < cols; c++){
+      thisGrid[r][c].display();
+    }
+  }
+}
+
---
+++ js/hide-tile.js
@@ -0,0 +1,47 @@
+class Tile {
+  constructor(x,y,col,row, hide = true, type = 0){
+    this.x = x;
+    this.y = y;
+    this.col = col;
+    this.row = row;
+    this.hide = hide;
+    this.type = type;
+    this.number = '';
+    this.debounce = 0;
+  }
+  display() {
+    let c = color(255,255,255);
+    if(!this.hide){
+      c = color(255,255,200);
+    }
+    fill(c);
+    rect(this.y, this.x, 40, 40);
+    fill(color(0,0,0));
+    if(typeof this.number == 'string'){
+      text(this.number, this.y+10,this.x+30);      
+    }
+    this.debounce -= 1;
+  }
+  show(delay){
+    setTimeout(this.unhideTile.bind(this), delay);
+    setTimeout(this.hideTile.bind(this), delay+500);
+  }
+  unhideTile(delay=100){
+    this.hide = false;
+  }
+  hideTile() {
+    this.hide = true;
+  }
+  click(){
+    let d = dist(mouseX,mouseY,this.y+25,this.x+25);
+    if(d < 25 && this.debounce <= 0){
+      this.number = prompt("#");
+      this.hide = false;
+      this.debounce = 100;
+    }
+  }
+  randomize(){
+    this.type = random([0,1]);
+    return this.type;
+  }
+}
